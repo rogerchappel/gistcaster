a2e14186af228146bc0e3e1472d420740bb8c371
Roger Chappel <roger@users.noreply.github.com>
2026-05-02 14:43:46 +1000

    feat: expose public module API

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export { briefMarkdown } from './brief.js';
+export { captureSource } from './capture.js';
+export { captureFile } from './file-capture.js';
+export { defaultLibraryDir, ensureLibrary, loadRecords, readLibrary, saveRecord } from './library.js';
+export { qualificationMarkdown } from './qualification.js';
+export { captureUrl, htmlToText, parseUrl } from './url-capture.js';
