534f53b26397c7aea9dbcc8a2fbfa758672d0fce
Roger Chappel <miscanalysis@gmail.com>
2026-05-09 06:33:50 +1000

    feat: expose library API

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export { parseArgs, usage } from './args.js';
+export { buildBrief } from './brief.js';
+export { captureInputs } from './capture.js';
+export { renderBrief, renderOssIdeasQualification } from './exporters.js';
+export { writeBrief } from './library.js';
+export { isUrl, assertExplicitUrlFetch } from './safety.js';
